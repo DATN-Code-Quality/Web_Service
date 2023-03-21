0267a22c719205420366ecd23e0f41dfaf9fef92
trongkha256 <trongkha256@gmail.com>
2023-03-21 10:26:45 +0700

    confim yarn lock

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.TCP,
-  //   },
-  // );
-  // await app.listen();
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
 }
