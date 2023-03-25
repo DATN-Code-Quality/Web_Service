388f7584af7d8dd59eaf3ba402190f786271f836
tnhao <trannhathao1516@gmail.com>
2023-03-26 03:17:33 +0700

    feat: add get user detail from moodle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './temporal/client.module';
 import { UserModule } from './user/user.module';
+import { UserMoodleModule } from './userMoodle/userMoodle.module';
 
 @Module({
-  imports: [ClientModule, UserModule],
+  imports: [ClientModule, UserModule, UserMoodleModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/userMoodle/userMoodle.controller.ts
+++ src/userMoodle/userMoodle.controller.ts
@@ -1,15 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { ServiceResponse } from 'src/common/service-response';
+import { WorkflowClient } from '@temporalio/client';
+import { InjectTemporalClient } from 'nestjs-temporal';
+import { MOODLE_TASK_QUEUE } from 'src/config/workflowConstant';
+import { Workflows } from 'src/temporal/workflows';
 
 @ApiTags('User Moodle')
 @Controller('/api/user-moodle')
-export class UserController {
-  // constructor(private readonly appService: AppService) {}
+export class UserMoodleController {
+  constructor(
+    @InjectTemporalClient() private readonly client: WorkflowClient,
+  ) {}
 
   @Get('/get-user-by-email')
   async getUserByEmail() {
-    // return ServiceResponse.fromResult(result)
+    const handle = await this.client.start(Workflows.GetUsersByEmail, {
+      args: [['vuagio.2402@gmail.com']],
+      workflowId: `workflow-${new Date().getTime()}`,
+      taskQueue: MOODLE_TASK_QUEUE,
+    });
+    const result = await handle.result();
+    return result;
     // return await this.appService.getHello();
   }
 }
