994fa13e25319ede88e0d54fb255dcdaa0d77bb7
trongkha256 <trongkha256@gmail.com>
2023-04-15 14:15:13 +0700

    feat: add api create assignment
    Signed-off-by: trongkha256 <trongkha256@gmail.com>

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,19 +1,36 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import bcrypt from 'bcrypt';
 import {} from 'bcrypt';
 import { CategoryService } from './category.service';
 import { CategoryResDto } from './res/category-res.dto';
+import { CategoryReqDto } from './req/category-req.dto';
 const SALTROUNDS = 10;
 @ApiTags('Category')
 @Controller('/api/category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-
   @Get('/get-all-category')
   async getAllCategorys() {
     const result = await this.categoryService.findAll(CategoryResDto);
+    return result;
+  }
+
+  @Post('/create-categories')
+  async addCategories(@Body() categories: CategoryReqDto[]) {
+    const result = await this.categoryService.createMany(
+      CategoryResDto,
+      categories,
+    );
     return result;
   }
 }
